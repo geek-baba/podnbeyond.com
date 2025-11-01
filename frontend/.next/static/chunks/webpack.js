8d9c56d24706ae61e755c43992f33f3fe7845d20
Geek Baba <shwetkprabhat@gmail.com>
2025-11-01 09:56:46 -0400

    Add API connection test page for debugging

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b81059b4440fe8af"; }
+/******/ 		__webpack_require__.h = function() { return "86be2005de0d1cac"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
